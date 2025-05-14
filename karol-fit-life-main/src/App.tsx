335f91ce5231033a132001c3af8b8eb56dbcd14b
Henrique-R-Abduch <111626864+Henrique-R-Abduch@users.noreply.github.com>
2025-05-14 05:32:20 -0300

    Update App.tsx

--- karol-fit-life-main/src/App.tsx
+++ karol-fit-life-main/src/App.tsx
@@ -1,9 +1,9 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// Aqui importamos o HashRouter em vez do BrowserRouter:
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
@@ -20,17 +20,16 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        {/* Usamos o Router aqui */}
-        <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="workout" element={<Workout />} />
-            <Route path="diet" element={<Diet />} />
-            <Route path="profile" element={<Profile />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/workout" element={<Workout />} />
+            <Route path="/diet" element={<Diet />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
